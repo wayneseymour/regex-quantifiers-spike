cca933250632b5b9b9b2ed60855221ae9030c95f
Wayne Kenneth Seymour III <WayneSeymour@iheartmedia.com>
2017-07-06 22:56:20 -0400

    Added more tests from https://egghead.io/lessons/javascript-javascript-regular-expressions-find-repeated-patterns

--- __tests__/regex-quantifiers.spec.ts
+++ __tests__/regex-quantifiers.spec.ts
@@ -21,5 +21,25 @@ describe('regex quantifiers', () => {
 			expect(actual.every(r => typeof(r) === "string")).toBeTruthy();
 		});
 	});
-
+	describe('min / max matches using a quantifier', () => {
+		test(`should match at least 5 a's, onto infinity`, () => {
+			expect('aaaaaaaaaa'.match(/a{5,}/g)).toEqual(["aaaaaaaaaa"]);
+		});
+		test(`should match at least 5 a's, onto 6 a's`, () => {
+			expect('aaaaaaaaaa'.match(/a{5,6}/g)).toEqual(["aaaaaa"]);
+		});
+		test('should match on 3 items', () => {
+			const str = `
+http://egghead.io
+not a web address
+http://
+https://egghead.io
+httpss://egghead.io
+httpsss://egghead.io
+httpssss://egghead.io
+`
+			const regex = /https{2,4}:\/\//g;
+			expect(str.match(regex).length).toEqual(3);
+		});
+	})
 });
